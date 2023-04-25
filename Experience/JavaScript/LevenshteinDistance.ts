5547d0253689385b3d8a5aa8fa96638a54910ebd
AlexScriba <61542302+AlexScriba@users.noreply.github.com>
2023-04-25 13:51:18 +0800

    Added correct js experience implementation

--- Experience/JavaScript/LevenshteinDistance.ts
+++ Experience/JavaScript/LevenshteinDistance.ts
@@ -1,78 +1,118 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
 const levDist = (str1: string, str2: string) => {
-    const m = str1.length;
-    const n = str2.length;
+  const m = str1.length;
+  const n = str2.length;
 
-    if (m == 0) {
-        return m;
-    }
+  if (m == 0) {
+    return m;
+  }
 
-    if (n == 0) {
-        return m;
-    }
+  if (n == 0) {
+    return m;
+  }
 
-    const d: number[][] = [];
+  const d: number[][] = [];
 
-    for (let i = 0; i <= m; i++) {
-        let row = new Array(n + 1).fill(0);
-        d.push(row);
-    }
+  for (let i = 0; i <= m; i++) {
+    let row = new Array(n + 1).fill(0);
+    d.push(row);
+  }
 
+  for (let i = 1; i <= m; i++) {
+    d[i][0] = i;
+  }
+
+  for (let j = 1; j <= n; j++) {
+    d[0][j] = j;
+  }
+
+  for (let j = 1; j <= n; j++) {
     for (let i = 1; i <= m; i++) {
-        d[i][0] = i;
+      const substitutionCost =
+        str1.charAt(i - 1) === str2.charAt(j - 1) ? 0 : 1;
+
+      d[i][j] = Math.min(
+        d[i - 1][j] + 1,
+        d[i][j - 1] + 1,
+        d[i - 1][j - 1] + substitutionCost
+      );
     }
+  }
 
-    for (let j = 1; j <= n; j++) {
-        d[0][j] = j;
-    }
-
-    for (let j = 1; j <= n; j++) {
-        for (let i = 1; i <= m; i++) {
-            const substitutionCost = str1.charAt(i - 1) === str2.charAt(j - 1) ? 0 : 1;
-
-            d[i][j] = Math.min(
-                d[i - 1][j] + 1,
-                d[i][j - 1] + 1,
-                d[i - 1][j - 1] + substitutionCost
-            );
-        }
-    }
-
-    return d[m][n];
+  return d[m][n];
 };
 
-// TODO :: seems to have some bugs in output
-const main = () => {
-    const strings = fs.readFileSync('../strings.txt').toString().split('\n');
+interface CompResult {
+  i: number;
+  j: number;
+  distance: number;
+}
 
-    let comparisons = 0;
+const compare_distances = (
+  words: string[],
+  start: number,
+  end: number
+): CompResult[] => {
+  console.log(`Thread dispatched for ${start}-${end}`);
 
-    let closest = Infinity;
-    let indeces: [number, number] = [-1, -1];
-
-    for (let i = 0; i < strings.length - 1; i++) {
-        for (let j = i + 1; j < strings.length; j++) {
-            const str1 = strings[i];
-            const str2 = strings[j];
-
-            const dist = levDist(str1, str2);
-
-            if (dist < closest) {
-                closest = dist;
-                indeces = [i, j];
-            }
-
-            comparisons++;
-
-            if (comparisons % 1000 === 0) {
-                console.log('Comps:', comparisons);
-            }
-        }
+  let res: CompResult[] = [];
+  for (let i = start; i < end; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      let distance = levDist(words[i], words[j]);
+      res.push({ i, j, distance });
     }
+  }
 
-    console.log(`Closest: ${closest}\nIndeces: (${indeces[0]}, ${indeces[1]})`);
+  console.log(`Thread finished for ${start}-${end}`);
+
+  return res;
+};
+
+const main = async () => {
+  const args = process.argv;
+
+  if (args.length < 4)
+    throw new Error("Please provide arguments: <file_path> <num_threads>");
+
+  let file_path = args[2];
+  let num_threads = parseInt(args[3]);
+
+  const strings = fs
+    .readFileSync(file_path)
+    .toString()
+    .split("\n")
+    .filter((str) => str !== ""); // filter out empty lines
+
+  let chunk_size = Math.floor((strings.length + num_threads - 1) / num_threads);
+
+  // TS does not have threads
+  //  -> therefore using promises to mimick non blocking effect
+  let promises: Promise<CompResult[]>[] = [];
+  for (let i = 0; i < num_threads; i++) {
+    promises.push(
+      new Promise((resolve, _) => {
+        let start = i * chunk_size;
+        let end = Math.min((i + 1) * chunk_size, strings.length);
+
+        let distances = compare_distances(strings, start, end);
+        resolve(distances);
+      })
+    );
+  }
+
+  let responses = await Promise.all(promises);
+  let comparisons: CompResult[] = responses.flat();
+
+  let bestRes = comparisons.reduce((prev, curr) =>
+    curr.distance < prev.distance ? curr : prev
+  );
+
+  console.log(
+    `Most similar strings are: \n\t${strings[bestRes.i]}\n\t${
+      strings[bestRes.j]
+    }\nWith distance of ${bestRes.distance}`
+  );
 };
 
 main();
-
--- Experience/JavaScript/js/LevenshteinDistance.js
+++ Experience/JavaScript/js/LevenshteinDistance.js
@@ -22,11 +22,26 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const levDist = (str1, str2) => {
     const m = str1.length;
     const n = str2.length;
+    if (m == 0) {
+        return m;
+    }
+    if (n == 0) {
+        return m;
+    }
     const d = [];
     for (let i = 0; i <= m; i++) {
         let row = new Array(n + 1).fill(0);
@@ -46,26 +61,42 @@ const levDist = (str1, str2) => {
     }
     return d[m][n];
 };
-const main = () => {
-    const strings = fs.readFileSync('../strings.txt').toString().split('\n');
-    let comparisons = 0;
-    let closest = Infinity;
-    let indeces = [-1, -1];
-    for (let i = 0; i < strings.length - 1; i++) {
-        for (let j = i + 1; j < strings.length; j++) {
-            const str1 = strings[i];
-            const str2 = strings[2];
-            const dist = levDist(str1, str2);
-            if (dist < closest) {
-                closest = dist;
-                indeces = [i, j];
-            }
-            comparisons++;
-            if (comparisons % 1000 === 0) {
-                console.log('Comps:', comparisons);
-            }
+const compare_distances = (words, start, end) => {
+    console.log(`Thread dispatched for ${start}-${end}`);
+    let res = [];
+    for (let i = start; i < end; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            let distance = levDist(words[i], words[j]);
+            res.push({ i, j, distance });
         }
     }
-    console.log(`Closest: ${closest}\n Indeces: (${indeces[0]}, ${indeces[1]})`);
+    console.log(`Thread finished for ${start}-${end}`);
+    return res;
 };
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const args = process.argv;
+    if (args.length < 4)
+        throw new Error("Please provide arguments: <file_path> <num_threads>");
+    let file_path = args[2];
+    let num_threads = parseInt(args[3]);
+    const strings = fs
+        .readFileSync(file_path)
+        .toString()
+        .split("\n")
+        .filter((str) => str !== ""); // filter out any falsy values (like empty line)
+    let chunk_size = Math.floor((strings.length + num_threads - 1) / num_threads);
+    let promises = [];
+    for (let i = 0; i < num_threads; i++) {
+        promises.push(new Promise((resolve, _) => {
+            let start = i * chunk_size;
+            let end = Math.min((i + 1) * chunk_size, strings.length);
+            let distances = compare_distances(strings, start, end);
+            resolve(distances);
+        }));
+    }
+    let responses = yield Promise.all(promises);
+    let comparisons = responses.flat();
+    let bestRes = comparisons.reduce((prev, curr) => curr.distance < prev.distance ? curr : prev);
+    console.log(`Most similar strings are: \n\t${strings[bestRes.i]}\n\t${strings[bestRes.j]}\nWith distance of ${bestRes.distance}`);
+});
 main();
