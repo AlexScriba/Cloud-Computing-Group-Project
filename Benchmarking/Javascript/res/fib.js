ab7b0db9dacd0b49ce01695577ec5a7828686607
AlexScriba <61542302+AlexScriba@users.noreply.github.com>
2023-04-27 15:39:43 +0800

    TS fix

--- Benchmarking/Javascript/res/fib.js
+++ Benchmarking/Javascript/res/fib.js
@@ -8,7 +8,7 @@ let args = process.argv;
 if (args.length < 3)
     throw new Error("One parameter expected");
 let n = parseInt(args[2]);
-if (n === NaN)
+if (!n)
     throw new Error("Number format Incorrect");
 let res = fib_recursive(n);
 console.log(res);
